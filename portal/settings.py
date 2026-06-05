29fb509db0472d73411c1f67e47b73649d42c096
Yasaswi Yenduri <yasaswiyendluri@gmail.com>
2026-06-05 18:42:30 +0530

    fixes, deployment

--- portal/settings.py
+++ portal/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,11 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e08@^z7lyx7l-pgly&=q$)h^r-pl8#nve%z*f1*#bo0*(65l=8'
-
+SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 ALLOWED_HOSTS = []
 
 
